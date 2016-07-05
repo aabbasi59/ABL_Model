5571778369e0d0da15d06a2e792016eba259b5f7
Ali Abbasi <aliabbasi.civileng@gmail.com>
2016-07-05 19:26:28 +0200

    Update kEpsilon_buoyancy_lapse_mod.c

--- OF-Solver/kEpsilon_buoyanc/kEpsilon_buoyancy_lapse_mod.c
+++ OF-Solver/kEpsilon_buoyanc/kEpsilon_buoyancy_lapse_mod.c
@@ -1,1 +1,372 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2010 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "kEpsilon_buoyancy_lapse_mod.H"
+#include "addToRunTimeSelectionTable.H"
+
+#include "backwardsCompatibilityWallFunctions.H"
+#include "uniformDimensionedFields.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace incompressible
+{
+namespace RASModels
+{
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+defineTypeNameAndDebug(kEpsilon_buoyancy_lapse_mod, 0);
+addToRunTimeSelectionTable(RASModel, kEpsilon_buoyancy_lapse_mod, dictionary);
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+kEpsilon_buoyancy_lapse_mod::kEpsilon_buoyancy_lapse_mod
+(
+    const volVectorField& U,
+    const surfaceScalarField& phi,
+    transportModel& transport,
+    const word& turbulenceModelName,
+    const word& modelName
+)
+:
+    RASModel(modelName, U, phi, transport, turbulenceModelName),
+
+    Cmu_
+    (
+        dimensioned<scalar>::lookupOrAddToDict
+        (
+            "Cmu",
+            coeffDict_,
+            0.0333
+        )
+    ),
+    C1_
+    (
+        dimensioned<scalar>::lookupOrAddToDict
+        (
+            "C1",
+            coeffDict_,
+            1.176
+        )
+    ),
+    C2_
+    (
+        dimensioned<scalar>::lookupOrAddToDict
+        (
+            "C2",
+            coeffDict_,
+            1.92
+        )
+    ),
+    C3_
+    (
+        dimensioned<scalar>::lookupOrAddToDict
+        (
+            "C3",
+            coeffDict_,
+            -4.40
+        )
+    ),
+    sigmaEps_
+    (
+        dimensioned<scalar>::lookupOrAddToDict
+        (
+            "sigmaEps",
+            coeffDict_,
+            1.3
+        )
+    ),
+       sigmaK_
+    (
+        dimensioned<scalar>::lookupOrAddToDict
+        (
+            "sigmaK",
+            coeffDict_,
+            1.0
+        )
+    ),
+    TName_
+    (
+     coeffDict_.lookupOrDefault<word>("TName","T")
+    ),
+   beta_
+    (
+     transport.lookup("beta")
+    ),
+    Prt_
+    (
+     transport.lookup("Prt")
+    ),
+    g_gravity_
+    (
+     transport.lookup("g_gravity")
+    ),
+    cp_lapse_
+    (
+     transport.lookup("cp_lapse")
+    ),
+    k_
+    (
+        IOobject
+        (
+            "k",
+            runTime_.timeName(),
+            mesh_,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        autoCreateK("k", mesh_)
+    ),
+    epsilon_
+    (
+        IOobject
+        (
+            "epsilon",
+            runTime_.timeName(),
+            mesh_,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        autoCreateEpsilon("epsilon", mesh_)
+    ),
+    nut_
+    (
+        IOobject
+        (
+            "nut",
+            runTime_.timeName(),
+            mesh_,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        autoCreateNut("nut", mesh_)
+    )
+{
+    bound(k_, kMin_);
+    bound(epsilon_, epsilonMin_);
+    nut_ = Cmu_*sqr(k_)/(epsilon_);
+    nut_.correctBoundaryConditions();
+
+    printCoeffs();
+
+    if (debug)
+    {
+        Info 
+          << " TName : " << TName_
+          << nl
+          << " Prt : " << Prt_
+          << nl
+          << " beta : " << beta_
+          << endl;
+    }
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+tmp<volSymmTensorField> kEpsilon_buoyancy_lapse_mod::R() const
+{
+    return tmp<volSymmTensorField>
+    (
+        new volSymmTensorField
+        (
+            IOobject
+            (
+                "R",
+                runTime_.timeName(),
+                mesh_,
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            ),
+            ((2.0/3.0)*I)*k_ - nut_*twoSymm(fvc::grad(U_)),
+            k_.boundaryField().types()
+        )
+    );
+}
+
+
+tmp<volSymmTensorField> kEpsilon_buoyancy_lapse_mod::devReff() const
+{
+    return tmp<volSymmTensorField>
+    (
+        new volSymmTensorField
+        (
+            IOobject
+            (
+                "devRhoReff",
+                runTime_.timeName(),
+                mesh_,
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            ),
+           -nuEff()*dev(twoSymm(fvc::grad(U_)))
+        )
+    );
+}
+
+
+tmp<fvVectorMatrix> kEpsilon_buoyancy_lapse_mod::divDevReff(volVectorField& U) const
+{
+    return
+    (
+      - fvm::laplacian(nuEff(), U)
+      //- fvc::div(nuEff()*dev(T(fvc::grad(U))))
+      - fvc::div(nuEff()*dev(T(fvc::grad(U))))
+    );
+}
+
+tmp<fvVectorMatrix> kEpsilon_buoyancy_lapse_mod::divDevRhoReff
+(
+    const volScalarField& rho,
+    volVectorField& U
+) const
+{
+    volScalarField muEff("muEff", rho*nuEff());
+
+    return
+    (
+      - fvm::laplacian(muEff, U)
+      - fvc::div(muEff*dev(T(fvc::grad(U))))
+    );
+}
+
+
+bool kEpsilon_buoyancy_lapse_mod::read()
+{
+    if (RASModel::read())
+    {
+        Cmu_.readIfPresent(coeffDict());
+        C1_.readIfPresent(coeffDict());
+        C2_.readIfPresent(coeffDict());
+	C3_.readIfPresent(coeffDict());
+        sigmaEps_.readIfPresent(coeffDict());
+	sigmaK_.readIfPresent(coeffDict());
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+void kEpsilon_buoyancy_lapse_mod::correct()
+{
+    RASModel::correct();
+
+    if (!turbulence_)
+    {
+        return;
+    }
+
+    volScalarField G(GName(), nut_*2*magSqr(symm(fvc::grad(U_))));
+
+    const uniformDimensionedVectorField& g =
+        U_.db().lookupObject<uniformDimensionedVectorField>("g");
+    const volScalarField& T_ = U_.db().lookupObject<volScalarField>(TName_);
+
+    volScalarField Gb = nut_/Prt_*beta_*(g &(fvc::grad(T_)- g /cp_lapse_));
+    //volScalarField Gb = 0.5*(Bk+mag(Bk));
+
+    if (debug)
+    {
+        Info 
+          << " g : " << g 
+          << nl
+          << " T : " 
+          << " min: " << gMin(T_.internalField())
+          << " ave: " << T_.weightedAverage(mesh_.V()).value()
+          << " max: " << gMax(T_.internalField())
+          << nl
+          << " Bk : " 
+          << " min: " << gMin(Gb.internalField())
+          << " ave: " << Gb.weightedAverage(mesh_.V()).value()
+          << " max: " << gMax(Gb.internalField())
+          << nl
+          << " Bepsilon : " 
+          << " min: " << gMin(Gb.internalField())
+          << " ave: " << Gb.weightedAverage(mesh_.V()).value()
+          << " max: " << gMax(Gb.internalField())
+          << endl;
+    }
+
+    // Update espsilon and G at the wall
+    epsilon_.boundaryField().updateCoeffs();
+
+    // Dissipation equation
+    tmp<fvScalarMatrix> epsEqn
+    (
+        fvm::ddt(epsilon_)
+      + fvm::div(phi_, epsilon_)
+      - fvm::Sp(fvc::div(phi_), epsilon_)
+      - fvm::laplacian(DepsilonEff(), epsilon_)
+     ==
+        (C1_*G+C1_*Gb - C1_ * C3_ *Gb)*epsilon_/k_
+      - fvm::Sp(C2_*epsilon_/k_, epsilon_)
+    );
+
+    epsEqn().relax();
+
+    epsEqn().boundaryManipulate(epsilon_.boundaryField());
+
+    solve(epsEqn);
+    bound(epsilon_, epsilonMin_);
+
+
+    // Turbulent kinetic energy equation
+    tmp<fvScalarMatrix> kEqn
+    (
+        fvm::ddt(k_)
+      + fvm::div(phi_, k_)
+      - fvm::Sp(fvc::div(phi_), k_)
+      - fvm::laplacian(DkEff(), k_)
+     ==
+        G+Gb
+      - fvm::Sp(epsilon_/k_, k_)
+    );
+
+    kEqn().relax();
+    solve(kEqn);
+    bound(k_, kMin_);
+
+
+    // Re-calculate viscosity
+    nut_ = Cmu_*sqr(k_)/epsilon_;
+    nut_.correctBoundaryConditions();
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace RASModels
+} // End namespace incompressible
+} // End namespace Foam
+
+// ************************************************************************* //
 
